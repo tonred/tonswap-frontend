4b765a8cde938bfe90607bf7a52f5217d1d24e6d
Pavel Ivanov <sudokey@gmail.com>
2021-11-20 16:08:56 +0000

    Added Remove Liquidity button to pools overview page

--- src/modules/Pools/index.tsx
+++ src/modules/Pools/index.tsx
@@ -15,14 +15,20 @@ export function Pools(): JSX.Element {
     return (
         <div className="container container--large">
             <section className="section">
-                <div className="section__header">
+                <div className="section__header section__header_wrap">
                     <SectionTitle>
                         {intl.formatMessage({ id: 'POOLS_LIST_TITLE' })}
                     </SectionTitle>
 
-                    <Link to="/pool" className="btn btn-primary">
-                        {intl.formatMessage({ id: 'POOLS_LIST_HEADER_BUTTON' })}
-                    </Link>
+                    <div className="section__header-actions">
+                        <Link to="/pools/burn-liquidity" className="btn btn-secondary">
+                            {intl.formatMessage({ id: 'POOLS_LIST_HEADER_REMOVE' })}
+                        </Link>
+
+                        <Link to="/pool" className="btn btn-primary">
+                            {intl.formatMessage({ id: 'POOLS_LIST_HEADER_NEW' })}
+                        </Link>
+                    </div>
                 </div>
                 <WalletConnector
                     message={intl.formatMessage({ id: 'POOLS_LIST_CONNECT_WALLET_TITLE' })}
